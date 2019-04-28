956f1fd1f937985ddcd6d73170607ebf25065711
bless4 <kwangmart12@gmail.com>
2019-04-28 11:13:08 +0200

    #03 Creat Venue NFO Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './resources/styles.css';
-
 import Header from './components/header_footer/Header';
 import Featured from './components/featured';
+import VenueNfo from './components/venueNfo/venueNfo'
 
 class App extends Component {
   render() {
@@ -10,6 +10,7 @@ class App extends Component {
       <div className="App" style={{ height:"1500px",background:'cornflowerblue'}}>
         <Header/>
         <Featured/>
+        <VenueNfo/>
       </div>
     );
   }
